5a878574c5e1801ff531e3bede3a2618c0b3fd35
Enzo SALVATI - babi <95984938+babise@users.noreply.github.com>
2024-07-01 22:37:47 +0200

    Apostrophe build

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -5,17 +5,17 @@ export default function About() {
 		<div className={styles.container}>
 			<div className={styles.text}>
 				<p>
-					Salut, moi c'est Thibault Grall (a.k.a Katem), designer graphique
-					passionné. Aventurier du design en général, j'explore et m'essaye à
-					tout type de création. Dans ce portfolio, vous découvrirez différents
-					projets issus de divers domaines. La plupart sont des créations
-					personnelles, mais vous trouverez également des travaux professionnels
-					et scolaires.
+					Salut, moi c&apos;est Thibault Grall (a.k.a Katem), designer graphique
+					passionné. Aventurier du design en général, j&apos;explore et
+					m&apos;essaye à tout type de création. Dans ce portfolio, vous
+					découvrirez différents projets issus de divers domaines. La plupart
+					sont des créations personnelles, mais vous trouverez également des
+					travaux professionnels et scolaires.
 				</p>
 				<p>
 					<strong>
 						<br />
-						Je suis actuellement à la recherche d'une alternance en design
+						Je suis actuellement à la recherche d&apos;une alternance en design
 						graphique pour ma première année de Mastère !
 					</strong>
 				</p>
